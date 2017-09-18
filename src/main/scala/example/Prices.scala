61acec48dfe46e820755f33e8205fa3175d7f230
Tom Liddle <tliddle@brighttalk.com>
2017-09-18 21:49:38 +0100

    Daily prices method added + test

--- src/main/scala/example/Prices.scala
+++ src/main/scala/example/Prices.scala
@@ -1,11 +1,17 @@
 package example
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-object Prices extends App {
+
+object Prices {
 
 
-  def pricesURL(ticker: String): String = s"https://www.google.com/finance/historical?q=NASDAQ:$ticker&output=csv"
 
+  // Format is 9-Aug-17
+  private final val formatter = DateTimeFormatter.ofPattern("d-MMM-yy")
+
+  private def pricesURL(ticker: String): String = s"https://www.google.com/finance/historical?q=NASDAQ:$ticker&output=csv"
 
 
   //Task
@@ -14,36 +20,34 @@ object Prices extends App {
 
 
 
-  /* 1 - 1 year historic prices given a ticker */
-  def dailyPrices(ticker: String): Seq[(String, Double)] = {
-
+  /**
+    * 1 - 1 year historic prices given a ticker
+    * @param ticker e.g. GOOG
+    * @return Seq of tuple containing Date and Price
+    */
+  def dailyPrices(ticker: String): Seq[(LocalDate, Double)] = {
+    scala.io.Source.fromURL(pricesURL(ticker)).getLines.drop(1).map { l =>
+      val t = l.split(",")
+      (LocalDate.parse(t(0), formatter), t(4).toDouble)
+    }.toSeq
   }
 
 
 
   /* 2- daily returns, where return = ( Price_Today – Price_Yesterday)/Price_Yesterday */
 
-  def returns(ticker:String): Seq[(String, Double)] = {
+  /*def returns(ticker: String): Seq[(LocalDate, Double)] = {
 
-  }
+  }*/
 
 
 
   /* 3 – 1 year mean returns */
 
-  def meanReturn(ticker:String): Double = {
+  /*def meanReturn(ticker:String): Double = {
 
-  }
+  }*/
 
-
-
-  /* example usage */
-
-  val googleDailyPrices = dailyPrices("GOOG")
-
-  val googleDailyReturns = returns("GOOG")
-
-  val googleAverageReturns = meanReturn("GOOG")
 }
 
 
--- src/test/scala/example/PricesSpec.scala
+++ src/test/scala/example/PricesSpec.scala
@@ -4,7 +4,10 @@ import org.scalatest._
 
 class PricesSpec extends FlatSpec with Matchers {
 
-  "The Prices object" should "" in {
+  "The Prices object" should "get the historic prices" in {
 
+    val prices =  Prices.dailyPrices("GOOG")
+
+    prices.size should be(100)
   }
 }
